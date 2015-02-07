ab4f1be2dddd8e4ffa31e31855cb7873b88879f0
Andrew Ripka <aripka@pivotal.io>
2015-02-06 21:00:21 -0600

    implement a system exit function

--- hello.go
+++ hello.go
@@ -11,17 +11,21 @@ func handler(w http.ResponseWriter, r *http.Request){
 	fmt.Fprintf(w, "Hello, request received at %s", r.URL.Path[1:])
 }
 
-
+func exitNow(w http.ResponseWriter, r *http.Request){
+	log.Println("exit called")
+	os.Exit(-1)
+}
 
 func main(){
 	http.HandleFunc("/", handler)
+	http.HandleFunc("/exit", exitNow)
 	port_num := os.Getenv("PORT")
 	if port_num == ""{
 		port_num = "8080"
 	}
 	port := ":" + port_num
-	fmt.Printf("Listening on port %s\n", port)
-	fmt.Printf("****Cheat commandos, ROCK ROCK ON!!!!") 
+	log.Printf("Listening on port %s\n", port)
+	log.Println("****Cheat commandos, ROCK ROCK ON!!!!") 
 	err := http.ListenAndServe(port, nil)
 
 	if err != nil {
